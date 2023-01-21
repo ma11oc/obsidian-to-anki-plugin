9d09d1f4afe3fb82cb81c78f785b2cea9bd7c4ba
Alexey Shchukin <alex.v.schukin@gmail.com>
2023-01-21 15:43:13 +0400

    add basic callout support

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-new */
+
 import { Notice, Plugin, addIcon } from 'obsidian'
 import * as AnkiConnect from './src/anki'
 import { PluginSettings, ParsedSettings } from './src/interfaces/settings-interface'
@@ -6,225 +8,223 @@ import { ANKI_ICON } from './src/constants'
 import { settingToData } from './src/setting-to-data'
 import { FileManager } from './src/files-manager'
 
-export default class MyPlugin extends Plugin {
+export default class ObsidianToAnki extends Plugin {
+  settings: PluginSettings
+  note_types: Array<string>
+  fields_dict: Record<string, string[]>
+  added_media: string[]
+  file_hashes: Record<string, string>
 
-	settings: PluginSettings
-	note_types: Array<string>
-	fields_dict: Record<string, string[]>
-	added_media: string[]
-	file_hashes: Record<string, string>
+  async getDefaultSettings (): Promise<PluginSettings> {
+    const settings: PluginSettings = {
+      CUSTOM_REGEXPS: {},
+      FILE_LINK_FIELDS: {},
+      CONTEXT_FIELDS: {},
+      FOLDER_DECKS: {},
+      FOLDER_TAGS: {},
+      Syntax: {
+        'Begin Note': 'START',
+        'End Note': 'END',
+        'Begin Inline Note': 'STARTI',
+        'End Inline Note': 'ENDI',
+        'Begin Callout Note': '> [!anki]',
+        'End Callout Note': '^$',
+        'Target Deck Line': 'TARGET DECK',
+        'File Tags Line': 'FILE TAGS',
+        'Delete Note Line': 'DELETE',
+        'Frozen Fields Line': 'FROZEN'
+      },
+      Defaults: {
+        Tag: 'Obsidian_to_Anki',
+        Deck: 'Default',
+        'Scheduling Interval': 0,
+        'Add File Link': false,
+        'Add Context': false,
+        CurlyCloze: false,
+        'CurlyCloze - Highlights to Clozes': false,
+        'ID Comments': true,
+        'Add Obsidian Tags': false
+      }
+    }
+    /* Making settings from scratch, so need note types */
+    this.note_types = await AnkiConnect.invoke('modelNames') as Array<string>
+    this.fields_dict = await this.generateFieldsDict()
+    for (const note_type of this.note_types) {
+      settings.CUSTOM_REGEXPS[note_type] = ''
+      const field_names: string[] = await AnkiConnect.invoke(
+        'modelFieldNames', { modelName: note_type }
+      ) as string[]
+      this.fields_dict[note_type] = field_names
+      settings.FILE_LINK_FIELDS[note_type] = field_names[0]
+    }
+    return settings
+  }
 
-	async getDefaultSettings(): Promise<PluginSettings> {
-		let settings: PluginSettings = {
-			CUSTOM_REGEXPS: {},
-			FILE_LINK_FIELDS: {},
-			CONTEXT_FIELDS: {},
-			FOLDER_DECKS: {},
-			FOLDER_TAGS: {},
-			Syntax: {
-				"Begin Note": "START",
-				"End Note": "END",
-				"Begin Inline Note": "STARTI",
-				"End Inline Note": "ENDI",
-				"Target Deck Line": "TARGET DECK",
-				"File Tags Line": "FILE TAGS",
-				"Delete Note Line": "DELETE",
-				"Frozen Fields Line": "FROZEN"
-			},
-			Defaults: {
-				"Tag": "Obsidian_to_Anki",
-				"Deck": "Default",
-				"Scheduling Interval": 0,
-				"Add File Link": false,
-				"Add Context": false,
-				"CurlyCloze": false,
-				"CurlyCloze - Highlights to Clozes": false,
-				"ID Comments": true,
-				"Add Obsidian Tags": false,
-			}
-		}
-		/*Making settings from scratch, so need note types*/
-		this.note_types = await AnkiConnect.invoke('modelNames') as Array<string>
-		this.fields_dict = await this.generateFieldsDict()
-		for (let note_type of this.note_types) {
-			settings["CUSTOM_REGEXPS"][note_type] = ""
-			const field_names: string[] = await AnkiConnect.invoke(
-	            'modelFieldNames', {modelName: note_type}
-	        ) as string[]
-			this.fields_dict[note_type] = field_names
-			settings["FILE_LINK_FIELDS"][note_type] = field_names[0]
-		}
-		return settings
-	}
+  async generateFieldsDict (): Promise<Record<string, string[]>> {
+    const fields_dict = {}
+    for (const note_type of this.note_types) {
+      const field_names: string[] = await AnkiConnect.invoke(
+        'modelFieldNames', { modelName: note_type }
+      ) as string[]
+      fields_dict[note_type] = field_names
+    }
+    return fields_dict
+  }
 
-	async generateFieldsDict(): Promise<Record<string, string[]>> {
-		let fields_dict = {}
-		for (let note_type of this.note_types) {
-			const field_names: string[] = await AnkiConnect.invoke(
-				'modelFieldNames', {modelName: note_type}
-			) as string[]
-			fields_dict[note_type] = field_names
-		}
-		return fields_dict
-	}
+  async saveDefault (): Promise<void> {
+    const default_sets = await this.getDefaultSettings()
+    this.saveData(
+      {
+        settings: default_sets,
+        'Added Media': [],
+        'File Hashes': {},
+        fields_dict: {}
+      }
+    )
+  }
 
-	async saveDefault(): Promise<void> {
-		const default_sets = await this.getDefaultSettings()
-		this.saveData(
-			{
-				settings: default_sets,
-				"Added Media": [],
-				"File Hashes": {},
-				fields_dict: {}
-			}
-		)
-	}
+  async loadSettings (): Promise<PluginSettings> {
+    const current_data = await this.loadData()
+    if (current_data == null || Object.keys(current_data).length !== 4) {
+      new Notice('Need to connect to Anki generate default settings...')
+      const default_sets = await this.getDefaultSettings()
+      this.saveData(
+        {
+          settings: default_sets,
+          'Added Media': [],
+          'File Hashes': {},
+          fields_dict: {}
+        }
+      )
+      new Notice('Default settings successfully generated!')
+      return default_sets
+    } else {
+      return current_data.settings
+    }
+  }
 
-	async loadSettings(): Promise<PluginSettings> {
-		let current_data = await this.loadData()
-		if (current_data == null || Object.keys(current_data).length != 4) {
-			new Notice("Need to connect to Anki generate default settings...")
-			const default_sets = await this.getDefaultSettings()
-			this.saveData(
-				{
-					settings: default_sets,
-					"Added Media": [],
-					"File Hashes": {},
-					fields_dict: {}
-				}
-			)
-			new Notice("Default settings successfully generated!")
-			return default_sets
-		} else {
-			return current_data.settings
-		}
-	}
+  async loadAddedMedia (): Promise<string[]> {
+    const current_data = await this.loadData()
+    if (current_data == null) {
+      await this.saveDefault()
+      return []
+    } else {
+      return current_data['Added Media']
+    }
+  }
 
-	async loadAddedMedia(): Promise<string[]> {
-		let current_data = await this.loadData()
-		if (current_data == null) {
-			await this.saveDefault()
-			return []
-		} else {
-			return current_data["Added Media"]
-		}
-	}
+  async loadFileHashes (): Promise<Record<string, string>> {
+    const current_data = await this.loadData()
+    if (current_data == null) {
+      await this.saveDefault()
+      return {}
+    } else {
+      return current_data['File Hashes']
+    }
+  }
 
-	async loadFileHashes(): Promise<Record<string, string>> {
-		let current_data = await this.loadData()
-		if (current_data == null) {
-			await this.saveDefault()
-			return {}
-		} else {
-			return current_data["File Hashes"]
-		}
-	}
+  async loadFieldsDict (): Promise<Record<string, string[]>> {
+    const current_data = await this.loadData()
+    if (current_data == null) {
+      await this.saveDefault()
+      const fields_dict = await this.generateFieldsDict()
+      return fields_dict
+    }
+    return current_data.fields_dict
+  }
 
-	async loadFieldsDict(): Promise<Record<string, string[]>> {
-		let current_data = await this.loadData()
-		if (current_data == null) {
-			await this.saveDefault()
-			const fields_dict = await this.generateFieldsDict()
-			return fields_dict
-		}
-		return current_data.fields_dict
-	}
+  async saveAllData (): Promise<void> {
+    this.saveData(
+      {
+        settings: this.settings,
+        'Added Media': this.added_media,
+        'File Hashes': this.file_hashes,
+        fields_dict: this.fields_dict
+      }
+    )
+  }
 
-	async saveAllData(): Promise<void> {
-		this.saveData(
-				{
-					settings: this.settings,
-					"Added Media": this.added_media,
-					"File Hashes": this.file_hashes,
-					fields_dict: this.fields_dict
-				}
-		)
-	}
+  regenerateSettingsRegexps () {
+    const regexp_section = this.settings.CUSTOM_REGEXPS
+    // For new note types
+    for (const note_type of this.note_types) {
+      this.settings.CUSTOM_REGEXPS[note_type] = regexp_section.hasOwnProperty(note_type) ? regexp_section[note_type] : ''
+    }
+    // Removing old note types
+    for (const note_type of Object.keys(this.settings.CUSTOM_REGEXPS)) {
+      if (!this.note_types.includes(note_type)) {
+        delete this.settings.CUSTOM_REGEXPS[note_type]
+      }
+    }
+  }
 
-	regenerateSettingsRegexps() {
-		let regexp_section = this.settings["CUSTOM_REGEXPS"]
-		// For new note types
-		for (let note_type of this.note_types) {
-			this.settings["CUSTOM_REGEXPS"][note_type] = regexp_section.hasOwnProperty(note_type) ? regexp_section[note_type] : ""
-		}
-		// Removing old note types
-		for (let note_type of Object.keys(this.settings["CUSTOM_REGEXPS"])) {
-			if (!this.note_types.includes(note_type)) {
-				delete this.settings["CUSTOM_REGEXPS"][note_type]
-			}
-		}
-	}
+  async scanVault () {
+    new Notice('Scanning vault, check console for details...')
+    console.info('Checking connection to Anki...')
+    try {
+      await AnkiConnect.invoke('modelNames')
+    } catch (e) {
+      new Notice("Error, couldn't connect to Anki! Check console for error message.")
+      return
+    }
+    new Notice("Successfully connected to Anki! This could take a few minutes - please don't close Anki until the plugin is finished")
+    const data: ParsedSettings = await settingToData(this.app, this.settings, this.fields_dict)
+    const manager = new FileManager(this.app, data, this.app.vault.getMarkdownFiles(), this.file_hashes, this.added_media)
+    await manager.initialiseFiles()
+    await manager.requests_1()
+    this.added_media = Array.from(manager.added_media_set)
+    const hashes = manager.getHashes()
+    for (const key in hashes) {
+      this.file_hashes[key] = hashes[key]
+    }
+    new Notice('All done! Saving file hashes and added media now...')
+    this.saveAllData()
+  }
 
-	async scanVault() {
-		new Notice('Scanning vault, check console for details...');
-		console.info("Checking connection to Anki...")
-		try {
-			await AnkiConnect.invoke('modelNames')
-		}
-		catch(e) {
-			new Notice("Error, couldn't connect to Anki! Check console for error message.")
-			return
-		}
-		new Notice("Successfully connected to Anki! This could take a few minutes - please don't close Anki until the plugin is finished")
-		const data: ParsedSettings = await settingToData(this.app, this.settings, this.fields_dict)
-		const manager = new FileManager(this.app, data, this.app.vault.getMarkdownFiles(), this.file_hashes, this.added_media)
-		await manager.initialiseFiles()
-		await manager.requests_1()
-		this.added_media = Array.from(manager.added_media_set)
-		const hashes = manager.getHashes()
-		for (let key in hashes) {
-			this.file_hashes[key] = hashes[key]
-		}
-		new Notice("All done! Saving file hashes and added media now...")
-		this.saveAllData()
-	}
+  async onload () {
+    console.log('loading ObsidianToAnki...')
+    addIcon('anki', ANKI_ICON)
 
-	async onload() {
-		console.log('loading Obsidian_to_Anki...');
-		addIcon('anki', ANKI_ICON)
+    try {
+      this.settings = await this.loadSettings()
+    } catch (e) {
+      new Notice("Couldn't connect to Anki! Check console for error message.")
+      return
+    }
 
-		try {
-			this.settings = await this.loadSettings()
-		}
-		catch(e) {
-			new Notice("Couldn't connect to Anki! Check console for error message.")
-			return
-		}
+    this.note_types = Object.keys(this.settings.CUSTOM_REGEXPS)
+    this.fields_dict = await this.loadFieldsDict()
+    if (Object.keys(this.fields_dict).length === 0) {
+      new Notice('Need to connect to Anki to generate fields dictionary...')
+      try {
+        this.fields_dict = await this.generateFieldsDict()
+        new Notice('Fields dictionary successfully generated!')
+      } catch (e) {
+        new Notice("Couldn't connect to Anki! Check console for error message.")
+        return
+      }
+    }
+    this.added_media = await this.loadAddedMedia()
+    this.file_hashes = await this.loadFileHashes()
 
-		this.note_types = Object.keys(this.settings["CUSTOM_REGEXPS"])
-		this.fields_dict = await this.loadFieldsDict()
-		if (Object.keys(this.fields_dict).length == 0) {
-			new Notice('Need to connect to Anki to generate fields dictionary...')
-			try {
-				this.fields_dict = await this.generateFieldsDict()
-				new Notice("Fields dictionary successfully generated!")
-			}
-			catch(e) {
-				new Notice("Couldn't connect to Anki! Check console for error message.")
-				return
-			}
-		}
-		this.added_media = await this.loadAddedMedia()
-		this.file_hashes = await this.loadFileHashes()
+    this.addSettingTab(new SettingsTab(this.app, this))
 
-		this.addSettingTab(new SettingsTab(this.app, this));
+    this.addRibbonIcon('anki', 'ObsidianToAnki - Scan Vault', async () => {
+      await this.scanVault()
+    })
 
-		this.addRibbonIcon('anki', 'Obsidian_to_Anki - Scan Vault', async () => {
-			await this.scanVault()
-		})
+    this.addCommand({
+      id: 'anki-scan-vault',
+      name: 'Scan Vault',
+      callback: async () => {
+        await this.scanVault()
+      }
+    })
+  }
 
-		this.addCommand({
-			id: 'anki-scan-vault',
-			name: 'Scan Vault',
-			callback: async () => {
-			 	await this.scanVault()
-			 }
-		})
-	}
-
-	async onunload() {
-		console.log("Saving settings for Obsidian_to_Anki...")
-		this.saveAllData()
-		console.log('unloading Obsidian_to_Anki...');
-	}
+  async onunload () {
+    console.log('Saving settings for ObsidianToAnki...')
+    this.saveAllData()
+    console.log('Unloading ObsidianToAnki...')
+  }
 }
--- src/format.ts
+++ src/format.ts
@@ -1,6 +1,6 @@
 import { AnkiConnectNote } from './interfaces/note-interface'
 import { basename, extname } from 'path'
-import { Converter } from 'showdown'
+import { Converter, ShowdownExtension } from 'showdown'
 import { CachedMetadata } from 'obsidian'
 import * as c from './constants'
 
@@ -9,175 +9,171 @@ import showdownHighlight from 'showdown-highlight'
 const ANKI_MATH_REGEXP:RegExp = /(\\\[[\s\S]*?\\\])|(\\\([\s\S]*?\\\))/g
 const HIGHLIGHT_REGEXP:RegExp = /==(.*?)==/g
 
-const MATH_REPLACE:string = "OBSTOANKIMATH"
-const INLINE_CODE_REPLACE:string = "OBSTOANKICODEINLINE"
-const DISPLAY_CODE_REPLACE:string = "OBSTOANKICODEDISPLAY"
+const MATH_REPLACE:string = 'OBSTOANKIMATH'
+const INLINE_CODE_REPLACE:string = 'OBSTOANKICODEINLINE'
+const DISPLAY_CODE_REPLACE:string = 'OBSTOANKICODEDISPLAY'
 
 const CLOZE_REGEXP:RegExp = /(?:(?<!{){(?:c?(\d+)[:|])?(?!{))((?:[^\n][\n]?)+?)(?:(?<!})}(?!}))/g
 
-const IMAGE_EXTS: string[] = [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".tiff"]
-const AUDIO_EXTS: string[] = [".wav", ".m4a", ".flac", ".mp3", ".wma", ".aac", ".webm"]
+const IMAGE_EXTS: string[] = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.svg', '.tiff']
+const AUDIO_EXTS: string[] = ['.wav', '.m4a', '.flac', '.mp3', '.wma', '.aac', '.webm']
 
-const PARA_OPEN:string = "<p>"
-const PARA_CLOSE:string = "</p>"
+const PARA_OPEN:string = '<p>'
+const PARA_CLOSE:string = '</p>'
 
-let cloze_unset_num: number = 1
+let clozeUnsetNum: number = 1
 
-let converter: Converter = new Converter({
-	simplifiedAutoLink: true,
-	literalMidWordUnderscores: true,
-	tables: true, tasklists: true,
-	simpleLineBreaks: true,
-	requireSpaceBeforeHeadingText: true,
-	extensions: [showdownHighlight]
+const converter: Converter = new Converter({
+  extensions: [showdownHighlight as unknown as ShowdownExtension[]],
+  literalMidWordUnderscores: true,
+  requireSpaceBeforeHeadingText: true,
+  simpleLineBreaks: true,
+  simplifiedAutoLink: true,
+  tables: true,
+  tasklists: true
 })
 
-function escapeHtml(unsafe: string): string {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
- }
+function escapeHtml (unsafe: string): string {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
 
 export class FormatConverter {
+  fileCache: CachedMetadata
+  vaultName: string
+  detectedMedia: Set<string>
 
-	file_cache: CachedMetadata
-	vault_name: string
-	detectedMedia: Set<string>
+  constructor (fileCache: CachedMetadata, vaultName: string) {
+    this.vaultName = vaultName
+    this.fileCache = fileCache
+    this.detectedMedia = new Set()
+  }
 
-	constructor(file_cache: CachedMetadata, vault_name: string) {
-		this.vault_name = vault_name
-		this.file_cache = file_cache
-		this.detectedMedia = new Set()
-	}
+  getUrlFromLink (link: string): string {
+    return 'obsidian://open?vault=' + encodeURIComponent(this.vaultName) + String.raw`&file=` + encodeURIComponent(link)
+  }
 
-	getUrlFromLink(link: string): string {
-        return "obsidian://open?vault=" + encodeURIComponent(this.vault_name) + String.raw`&file=` + encodeURIComponent(link)
+  format_note_with_url (note: AnkiConnectNote, url: string, field: string): void {
+    note.fields[field] += '<br><a href="' + url + '" class="obsidian-link">Obsidian</a>'
+  }
+
+  format_note_with_frozen_fields (note: AnkiConnectNote, frozenFieldsDict: Record<string, Record<string, string>>): void {
+    for (const field in note.fields) {
+      note.fields[field] += frozenFieldsDict[note.modelName][field]
     }
+  }
 
-	format_note_with_url(note: AnkiConnectNote, url: string, field: string): void {
-		note.fields[field] += '<br><a href="' + url + '" class="obsidian-link">Obsidian</a>'
-	}
+  obsidian_to_anki_math (noteText: string): string {
+    return noteText.replace(
+      c.OBS_DISPLAY_MATH_REGEXP, '\\[$1\\]'
+    ).replace(
+      c.OBS_INLINE_MATH_REGEXP,
+      '\\($1\\)'
+    )
+  }
 
-	format_note_with_frozen_fields(note: AnkiConnectNote, frozen_fields_dict: Record<string, Record<string, string>>): void {
-		for (let field in note.fields) {
-			note.fields[field] += frozen_fields_dict[note.modelName][field]
-		}
-	}
+  cloze_repl (_1: string, matchId: string, matchContent: string): string {
+    if (matchId === undefined) {
+      const result = '{{c' + clozeUnsetNum.toString() + '::' + matchContent + '}}'
+      // clozeUnsetNum += 1 // FIXME: make it flexible
+      return result
+    }
+    const result = '{{c' + matchId + '::' + matchContent + '}}'
+    return result
+  }
 
-	obsidian_to_anki_math(note_text: string): string {
-		return note_text.replace(
-				c.OBS_DISPLAY_MATH_REGEXP, "\\[$1\\]"
-		).replace(
-			c.OBS_INLINE_MATH_REGEXP,
-			"\\($1\\)"
-		)
-	}
+  curly_to_cloze (text: string): string {
+    /* Change text in curly brackets to Anki-formatted cloze. */
+    text = text.replace(CLOZE_REGEXP, this.cloze_repl)
+    clozeUnsetNum = 1
+    return text
+  }
 
-	cloze_repl(_1: string, match_id: string, match_content: string): string {
-		if (match_id == undefined) {
-			let result = "{{c" + cloze_unset_num.toString() + "::" + match_content + "}}"
-			cloze_unset_num += 1
-			return result
-		}
-		let result = "{{c" + match_id + "::" + match_content + "}}"
-		return result
-	}
+  getAndFormatMedias (noteText: string): string {
+    if (!(this.fileCache.hasOwnProperty('embeds'))) {
+      return noteText
+    }
+    for (const embed of this.fileCache.embeds) {
+      if (noteText.includes(embed.original)) {
+        this.detectedMedia.add(embed.link)
+        if (AUDIO_EXTS.includes(extname(embed.link))) {
+          noteText = noteText.replace(new RegExp(c.escapeRegex(embed.original), 'g'), '[sound:' + basename(embed.link) + ']')
+        } else if (IMAGE_EXTS.includes(extname(embed.link))) {
+          noteText = noteText.replace(
+            new RegExp(c.escapeRegex(embed.original), 'g'),
+            '<img src="' + basename(embed.link) + '" alt="' + embed.displayText + '">'
+          )
+        } else {
+          console.warn('Unsupported extension: ', extname(embed.link))
+        }
+      }
+    }
+    return noteText
+  }
 
-	curly_to_cloze(text: string): string {
-		/*Change text in curly brackets to Anki-formatted cloze.*/
-		text = text.replace(CLOZE_REGEXP, this.cloze_repl)
-		cloze_unset_num = 1
-		return text
-	}
+  formatLinks (noteText: string): string {
+    if (!(this.fileCache.hasOwnProperty('links'))) {
+      return noteText
+    }
+    for (const link of this.fileCache.links) {
+      noteText = noteText.replace(new RegExp(c.escapeRegex(link.original), 'g'), '<a href="' + this.getUrlFromLink(link.link) + '">' + link.displayText + '</a>')
+    }
+    return noteText
+  }
 
-	getAndFormatMedias(note_text: string): string {
-		if (!(this.file_cache.hasOwnProperty("embeds"))) {
-			return note_text
-		}
-		for (let embed of this.file_cache.embeds) {
-			if (note_text.includes(embed.original)) {
-				this.detectedMedia.add(embed.link)
-				if (AUDIO_EXTS.includes(extname(embed.link))) {
-					note_text = note_text.replace(new RegExp(c.escapeRegex(embed.original), "g"), "[sound:" + basename(embed.link) + "]")
-				} else if (IMAGE_EXTS.includes(extname(embed.link))) {
-					note_text = note_text.replace(
-						new RegExp(c.escapeRegex(embed.original), "g"),
-						'<img src="' + basename(embed.link) + '" alt="' + embed.displayText + '">'
-					)
-				} else {
-					console.warn("Unsupported extension: ", extname(embed.link))
-				}
-			}
-		}
-		return note_text
-	}
+  censor (noteText: string, regexp: RegExp, mask: string): [string, string[]] {
+    /* Take note_text and replace every match of regexp with mask, simultaneously adding it to a string array */
+    const matches: string[] = []
+    for (const match of noteText.matchAll(regexp)) {
+      matches.push(match[0])
+    }
+    return [noteText.replace(regexp, mask), matches]
+  }
 
-	formatLinks(note_text: string): string {
-		if (!(this.file_cache.hasOwnProperty("links"))) {
-			return note_text
-		}
-		for (let link of this.file_cache.links) {
-			note_text = note_text.replace(new RegExp(c.escapeRegex(link.original), "g"), '<a href="' + this.getUrlFromLink(link.link) + '">' + link.displayText + "</a>")
-		}
-		return note_text
-	}
+  decensor (noteText: string, mask:string, replacements: string[], escape: boolean): string {
+    for (const replacement of replacements) {
+      noteText = noteText.replace(
+        mask, escape ? escapeHtml(replacement) : replacement
+      )
+    }
+    return noteText
+  }
 
-	censor(note_text: string, regexp: RegExp, mask: string): [string, string[]] {
-		/*Take note_text and replace every match of regexp with mask, simultaneously adding it to a string array*/
-		let matches: string[] = []
-		for (let match of note_text.matchAll(regexp)) {
-			matches.push(match[0])
-		}
-		return [note_text.replace(regexp, mask), matches]
-	}
-
-	decensor(note_text: string, mask:string, replacements: string[], escape: boolean): string {
-		for (let replacement of replacements) {
-			note_text = note_text.replace(
-				mask, escape ? escapeHtml(replacement) : replacement
-			)
-		}
-		return note_text
-	}
-
-	format(note_text: string, cloze: boolean, highlights_to_cloze: boolean): string {
-		note_text = this.obsidian_to_anki_math(note_text)
-		//Extract the parts that are anki math
-		let math_matches: string[]
-		let inline_code_matches: string[]
-		let display_code_matches: string[]
-		const add_highlight_css: boolean = note_text.match(c.OBS_DISPLAY_CODE_REGEXP) ? true : false;
-		[note_text, math_matches] = this.censor(note_text, ANKI_MATH_REGEXP, MATH_REPLACE);
-		[note_text, display_code_matches] = this.censor(note_text, c.OBS_DISPLAY_CODE_REGEXP, DISPLAY_CODE_REPLACE);
-		[note_text, inline_code_matches] = this.censor(note_text, c.OBS_CODE_REGEXP, INLINE_CODE_REPLACE);
-		if (cloze) {
-			if (highlights_to_cloze) {
-				note_text = note_text.replace(HIGHLIGHT_REGEXP, "{$1}")
-			}
-			note_text = this.curly_to_cloze(note_text)
-		}
-		note_text = this.getAndFormatMedias(note_text)
-		note_text = this.formatLinks(note_text)
-		//Special for formatting highlights now, but want to avoid any == in code
-		note_text = note_text.replace(HIGHLIGHT_REGEXP, String.raw`<mark>$1</mark>`)
-		note_text = this.decensor(note_text, DISPLAY_CODE_REPLACE, display_code_matches, false)
-		note_text = this.decensor(note_text, INLINE_CODE_REPLACE, inline_code_matches, false)
-		note_text = converter.makeHtml(note_text)
-		note_text = this.decensor(note_text, MATH_REPLACE, math_matches, true).trim()
-		// Remove unnecessary paragraph tag
-		if (note_text.startsWith(PARA_OPEN) && note_text.endsWith(PARA_CLOSE)) {
-			note_text = note_text.slice(PARA_OPEN.length, -1 * PARA_CLOSE.length)
-		}
-		if (add_highlight_css) {
-			note_text = '<link href="' + c.CODE_CSS_URL + '" rel="stylesheet">' + note_text
-		}
-		return note_text
-	}
-
-
-
-
+  format (noteText: string, cloze: boolean, highlightToCloze: boolean): string {
+    noteText = this.obsidian_to_anki_math(noteText)
+    // Extract the parts that are anki math
+    let mathMatches: string[]
+    let inlineCodeMatches: string[]
+    let displayCodeMatches: string[]
+    const addHighlightCss: boolean = !!noteText.match(c.OBS_DISPLAY_CODE_REGEXP);
+    [noteText, mathMatches] = this.censor(noteText, ANKI_MATH_REGEXP, MATH_REPLACE);
+    [noteText, displayCodeMatches] = this.censor(noteText, c.OBS_DISPLAY_CODE_REGEXP, DISPLAY_CODE_REPLACE);
+    [noteText, inlineCodeMatches] = this.censor(noteText, c.OBS_CODE_REGEXP, INLINE_CODE_REPLACE)
+    if (cloze) {
+      if (highlightToCloze) {
+        noteText = noteText.replace(HIGHLIGHT_REGEXP, '{$1}')
+      }
+      noteText = this.curly_to_cloze(noteText)
+    }
+    noteText = this.getAndFormatMedias(noteText)
+    noteText = this.formatLinks(noteText)
+    // Special for formatting highlights now, but want to avoid any == in code
+    noteText = noteText.replace(HIGHLIGHT_REGEXP, String.raw`<mark>$1</mark>`)
+    noteText = this.decensor(noteText, DISPLAY_CODE_REPLACE, displayCodeMatches, false)
+    noteText = this.decensor(noteText, INLINE_CODE_REPLACE, inlineCodeMatches, false)
+    noteText = converter.makeHtml(noteText)
+    noteText = this.decensor(noteText, MATH_REPLACE, mathMatches, true).trim()
+    // Remove unnecessary paragraph tag
+    if (noteText.startsWith(PARA_OPEN) && noteText.endsWith(PARA_CLOSE)) {
+      noteText = noteText.slice(PARA_OPEN.length, -1 * PARA_CLOSE.length)
+    }
+    if (addHighlightCss) {
+      noteText = '<link href="' + c.CODE_CSS_URL + '" rel="stylesheet">' + noteText
+    }
+    return noteText
+  }
 }
